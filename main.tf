e9036a24bff901f30969135d8ea3f70ee3d74e3d
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-02-14 16:07:29 +0100

    Generate module usage via terraform-docs CI
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-/**
- * # terraform-tfe-mcaf-workspace
- *
- * MCAF Terraform module to create and manage a Terraform Cloud workspace.
- *
- * With default options Terraform will also create and manage a GitHub repository and attach it to the Terraform Cloud
- * workspace. If the `create_repository` option is set to `false`, the GitHub repository should already exist or the
- * Terraform run will fail.
- *
- */
-
 locals {
   connect_vcs_repo = var.connect_vcs_repo ? { create = true } : {}
 }
