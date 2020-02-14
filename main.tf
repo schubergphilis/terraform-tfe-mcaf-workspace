bf3dc635c258aeb29b58a8bf21e7fefcfa7b2993
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-02-14 13:30:42 +0100

    First version
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

---
+++ main.tf
@@ -0,0 +1,81 @@
+locals {
+  connect_vcs_repo = var.connect_vcs_repo ? { create = true } : {}
+}
+
+resource "tfe_workspace" "default" {
+  name                  = var.name
+  organization          = var.terraform_organization
+  auto_apply            = var.auto_apply
+  file_triggers_enabled = var.file_triggers_enabled
+  terraform_version     = var.terraform_version
+  trigger_prefixes      = var.trigger_prefixes
+  queue_all_runs        = true
+  working_directory     = var.working_directory
+
+  dynamic vcs_repo {
+    for_each = local.connect_vcs_repo
+
+    content {
+      identifier         = var.github_repository
+      branch             = var.branch
+      ingress_submodules = false
+      oauth_token_id     = var.oauth_token_id
+    }
+  }
+}
+
+resource "tfe_notification_configuration" "default" {
+  count                 = var.slack_notification_url != null ? 1 : 0
+  name                  = tfe_workspace.default.name
+  enabled               = true
+  destination_type      = "slack"
+  url                   = var.slack_notification_url
+  workspace_external_id = tfe_workspace.default.external_id
+
+  triggers = [
+    "run:created",
+    "run:planning",
+    "run:needs_attention",
+    "run:applying",
+    "run:completed",
+    "run:errored"
+  ]
+}
+
+resource "tfe_variable" "clear_text_env_variables" {
+  for_each = var.clear_text_env_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "env"
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "sensitive_env_variables" {
+  for_each = var.sensitive_env_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "clear_text_terraform_variables" {
+  for_each = var.clear_text_terraform_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "terraform"
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "sensitive_terraform_variables" {
+  for_each = var.sensitive_terraform_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "terraform"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
