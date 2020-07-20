00f9a11fce7eea67f872d8d921ae784400422165
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-07-20 23:19:40 +0200

    Fix deprecation warning
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- main.tf
+++ main.tf
@@ -25,12 +25,12 @@ resource "tfe_workspace" "default" {
 }
 
 resource "tfe_notification_configuration" "default" {
-  count                 = var.slack_notification_url != null ? 1 : 0
-  name                  = tfe_workspace.default.name
-  enabled               = true
-  destination_type      = "slack"
-  url                   = var.slack_notification_url
-  workspace_external_id = tfe_workspace.default.external_id
+  count            = var.slack_notification_url != null ? 1 : 0
+  name             = tfe_workspace.default.name
+  enabled          = true
+  destination_type = "slack"
+  url              = var.slack_notification_url
+  workspace_id     = tfe_workspace.default.external_id
 
   triggers = [
     "run:created",
