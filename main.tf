601214ad5fb52b8330ccfe9d731f38161a8c218a
Marwin Baumann <mbaumann@schubergphilis.com>
2024-01-30 13:47:59 +0100

    breaking: improve team access settings

--- main.tf
+++ main.tf
@@ -2,20 +2,24 @@ locals {
   connect_vcs_repo = var.repository_identifier != null ? { create = true } : {}
 }
 
+################################################################################
+# Workspace
+################################################################################
+
 resource "tfe_workspace" "default" {
   name                      = var.name
-  organization              = var.terraform_organization
   auto_apply                = var.auto_apply
   auto_apply_run_trigger    = var.auto_apply_run_trigger
   file_triggers_enabled     = var.file_triggers_enabled
   global_remote_state       = var.global_remote_state
+  organization              = var.terraform_organization
   project_id                = var.project_id
+  queue_all_runs            = var.queue_all_runs
   remote_state_consumer_ids = var.remote_state_consumer_ids
   ssh_key_id                = var.ssh_key_id
   tag_names                 = var.workspace_tags
   terraform_version         = var.terraform_version
   trigger_prefixes          = var.trigger_prefixes
-  queue_all_runs            = var.queue_all_runs
   working_directory         = var.working_directory
 
   dynamic "vcs_repo" {
@@ -46,14 +50,6 @@ resource "tfe_notification_configuration" "default" {
   triggers         = each.value.triggers
   url              = each.value.url
   workspace_id     = tfe_workspace.default.id
-}
-
-resource "tfe_team_access" "defautl" {
-  for_each = var.team_access
-
-  access       = each.value.access
-  team_id      = each.value.team_id
-  workspace_id = tfe_workspace.default.id
 }
 
 resource "tfe_variable" "clear_text_env_variables" {
@@ -113,4 +109,36 @@ resource "tfe_variable" "sensitive_terraform_variables" {
   category     = "terraform"
   sensitive    = true
   workspace_id = tfe_workspace.default.id
+}
+
+################################################################################
+# RBAC
+################################################################################
+
+data "tfe_team" "default" {
+  for_each = toset(keys(var.team_access))
+
+  name         = each.value
+  organization = var.terraform_organization
+}
+
+resource "tfe_team_access" "default" {
+  for_each = var.team_access
+
+  access       = each.value.access
+  team_id      = data.tfe_team.default[each.key].id
+  workspace_id = tfe_workspace.default.id
+
+  dynamic "permissions" {
+    for_each = each.value.permissions != null ? { create = true } : {}
+
+    content {
+      run_tasks         = each.value.permissions["run_tasks"]
+      runs              = each.value.permissions["runs"]
+      sentinel_mocks    = each.value.permissions["sentinel_mocks"]
+      state_versions    = each.value.permissions["state_versions"]
+      variables         = each.value.permissions["variables"]
+      workspace_locking = each.value.permissions["workspace_locking"]
+    }
+  }
 }
