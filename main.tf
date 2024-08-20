8ae87ad66929302f67a1cea8a9b8af6959982fba
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2024-08-20 12:45:10 +0200

    feature: Add support for variable sets
    Signed-off-by: Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>

--- main.tf
+++ main.tf
@@ -111,6 +111,13 @@ resource "tfe_variable" "sensitive_terraform_variables" {
   workspace_id = tfe_workspace.default.id
 }
 
+resource "tfe_workspace_variable_set" "default" {
+  for_each = var.variable_set_ids
+
+  variable_set_id = each.value
+  workspace_id    = tfe_workspace.default.id
+}
+
 ################################################################################
 # RBAC
 ################################################################################
