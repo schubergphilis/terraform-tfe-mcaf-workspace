24b8e20b4bde7152c9a7c64544fd0785748896a5
Stephen Hoekstra <shoekstra@schubergphilis.com>
2025-07-17 13:01:52 +0200

    chore: Update variable descriptions and outputs (#30)
    * chore: Update variable descriptions and outputs
    
    Updated variable descriptions and fixed ordering.
    
    Marked original `workspace_id` and `workspace_name` as deprecated and
    added `id` and `name` outputs. Being a workspace module, we don't need
    to include workspace in the name.
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>
    
    * docs(readme): update module usage
    
    ---------
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "workspace_name" {
+output "id" {
+  value       = tfe_workspace.default.id
+  description = "HCP Terraform workspace ID"
+}
+
+output "name" {
   value       = tfe_workspace.default.name
-  description = "The Terraform Cloud workspace name"
+  description = "HCP Terraform workspace name"
 }
 
 output "workspace_id" {
   value       = tfe_workspace.default.id
-  description = "The Terraform Cloud workspace ID"
+  description = "(**DEPRECATED**) HCP Terraform workspace ID"
+}
+
+output "workspace_name" {
+  value       = tfe_workspace.default.name
+  description = "(**DEPRECATED**) HCP Terraform workspace name"
 }
